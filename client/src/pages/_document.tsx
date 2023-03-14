59267a03dca6df1e22cb8e8422e888684bc4ee50
Dongju Kim <jook1356@gmail.com>
2023-03-14 17:49:24 +0900

    [Add]bookModal

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -1,3 +1,5 @@
+/** @jsxImportSource @emotion/react */
+import { jsx, css } from "@emotion/react";
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
@@ -5,9 +7,15 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body>
+        <div className="overlay-root" css={overlayCSS}/>
         <Main />
         <NextScript />
       </body>
     </Html>
   )
 }
+
+const overlayCSS = css`
+  z-index: 9999;
+  position: fixed;
+`
