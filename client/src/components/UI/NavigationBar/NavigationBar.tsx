f3b282f84b956c72b6b0abc75e4d61211d82411f
unknown <chopinoff@gmail.com>
2023-03-14 10:06:18 +0900

    [Modify] responsive

--- client/src/components/UI/NavigationBar/NavigationBar.tsx
+++ client/src/components/UI/NavigationBar/NavigationBar.tsx
@@ -1,21 +1,22 @@
-/** @jsxImportSource @emotion/react */
-import { useMediaQuery } from "react-responsive";
-import { useIsClient } from "@/components/Responsive/useIsClient";
+// import { useMediaQuery } from "react-responsive";
+// import { useIsClient } from "@/components/Responsive/useIsClient";
+import { isResponsive } from "@/components/Responsive/isResponsive";
 
 const NavigationBar = () => {
-  const isDesktop = useMediaQuery({
-    query: "(min-width: 1024px)",
-  });
-  const isTablet = useMediaQuery({
-    query: "(max-width: 1023px) and (min-width:768px)",
-  });
-  const isMobile = useMediaQuery({ query: "(max-width: 767px)" });
-  const isClient = useIsClient();
+  // const isDesktop = useMediaQuery({
+  //   query: "(min-width: 1024px)",
+  // });
+  // const isTablet = useMediaQuery({
+  //   query: "(max-width: 1023px) and (min-width:768px)",
+  // });
+  // const isMobile = useMediaQuery({ query: "(max-width: 767px)" });
+  // const isClient = useIsClient();
+  console.log(isResponsive);
   return (
     <div>
-      {isClient && isDesktop && <div>데스크탑</div>}
-      {isClient && isTablet && <div>태블릿</div>}
-      {isClient && isMobile && <div>모바일</div>}
+      {/* {responsive.isDesktop && <div>데스크탑</div>}
+      {responsive.isDesktop && <div>태블릿</div>}
+      {responsive.isDesktop && <div>모바일</div>} */}
     </div>
   );
 };
--- client/src/components/UI/ScrollableCarousel/Test.tsx
+++ client/src/components/UI/ScrollableCarousel/Test.tsx
@@ -1,38 +1,31 @@
-
 /** @jsxImportSource @emotion/react */
 import { jsx, css } from "@emotion/react";
-import { useState } from "react"
+import { useState } from "react";
 
 const Test = () => {
+  const add = () => {
+    setArray((prev) => [...prev, 0]);
+  };
 
-    const add = () => {
-        setArray((prev) => [...prev, 0])
-    }
+  const box = (
+    <div css={boxStyle}>
+      <button onClick={add}>추가!</button>
+    </div>
+  );
+  const [array, setArray] = useState([1, 2, 3, 4, 5]);
+  const boxTags = array.map(() => {
+    return box;
+  });
 
-    const box = <div css={boxStyle}><button onClick={add}>추가!</button></div>
-    const [array, setArray] = useState([1, 2, 3, 4, 5])
-    const boxTags = array.map(() => {
-        return box
-    })
-
-    
-
-    return (
-        <>
-            {boxTags}
-        </>
-        
-        
-    )
-}
+  return <>{boxTags}</>;
+};
 
 const boxStyle = css`
-    min-width: 100%;
-    height: 128px;
-    background-color: red;
-    border: 2px solid black;
-    box-sizing: border-box;
+  min-width: 100%;
+  height: 128px;
+  background-color: var(--main-color);
+  border: 2px solid black;
+  box-sizing: border-box;
+`;
 
-`
-
-export default Test
+export default Test;
