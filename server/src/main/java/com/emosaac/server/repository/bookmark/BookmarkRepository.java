fc71163cad86ff411ea1354939ef0fb87e0235e7
박찬희 <shine7065@naver.com>
2023-03-15 14:22:26 +0900

    [Setting] 소설, 웹툰 공통 레포지토리 생성

--- server/src/main/java/com/emosaac/server/repository/bookmark/BookmarkRepository.java
+++ server/src/main/java/com/emosaac/server/repository/bookmark/BookmarkRepository.java
@@ -1,4 +1,4 @@
-package com.emosaac.server.repository.novel;
+package com.emosaac.server.repository.bookmark;
 
 import com.emosaac.server.domain.book.BookMark;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,7 +7,7 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
-public interface NovelBookmarkRepository extends JpaRepository<BookMark, Long> {
+public interface BookmarkRepository extends JpaRepository<BookMark, Long> {
     @Query("select b from BookMark b where b.book.bookId = :bookId and b.user.userId = :userId")
     Optional<BookMark> existsByBookIdAndUserId(@Param("bookId") Long bookId, @Param("userId") Long userId);
 }
