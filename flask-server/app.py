0f56195231c2fcdc9fa9ab7153fe4abe2b37dba9
박찬희 <shine7065@naver.com>
2023-03-20 17:55:39 +0900

    [Init] Flask 서버 초기 세팅

--- flask-server/app.py
+++ flask-server/app.py
@@ -1,6 +1,12 @@
-from flask import Flask
+import os
+from flask import Flask, jsonify, request, Response
+from flask_cors import CORS
+import pandas as pd
+
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+app.config["CORS_SUPPORTS_CREDENTIALS"] = True
 
 @app.route('/')
-def hello_pybo():
-    return 'Hello, Pybo!'
+def index():
+    return '<p>안녕 이모작</p>'
