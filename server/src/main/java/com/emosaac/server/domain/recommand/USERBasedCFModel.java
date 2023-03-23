b1515053962853b043392426e29bd4575e9a1032
gms9424 <gms9424@naver.com>
2023-03-23 23:29:48 +0900

    [Feat] userbasedCf 도메인 추가

--- server/src/main/java/com/emosaac/server/domain/recommand/USERBasedCFModel.java
+++ server/src/main/java/com/emosaac/server/domain/recommand/USERBasedCFModel.java
@@ -1,4 +1,31 @@
 package com.emosaac.server.domain.recommand;
 
+import com.emosaac.server.domain.book.Book;
+import com.emosaac.server.domain.user.User;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name="USER_BASED_CF_MODEL")
 public class USERBasedCFModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="ITEM_NO")
+    private Integer id;
+
+    @JsonManagedReference
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name ="USER_NO")
+    private User user;
+
+    @JsonManagedReference
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name ="BOOK_NO")
+    private Book book;
 }
