83b7f5f70c1a77624d5db4fa37150bc97fd36102
박찬희 <shine7065@naver.com>
2023-03-24 09:34:51 +0900

    [Add] Settings.py에userbasedcf, userbasedpridict 앱 추가

--- server_django/userbasedpridict/parse.py
+++ server_django/userbasedpridict/parse.py
@@ -22,3 +22,13 @@ from recommand.models import UserPredictedGradeModel, User, Book, Score
 from django.db import connection
 
 # def BookListView():
+
+# ratings_arr.dot(item_sim_arr) : 평점 * 책 유사도
+# ratings_arr : 사용자 u의 아이템 i와 가장 유사도가 높은 Top_N개 아이템에 대한 실제 평점 벡터
+# item_sim_arr : 아이템 i와 가장 유사도가 높은 Top_N개 아이템의 유사도 벡터
+
+# 개인화된 예측 평점
+# 평점 value와 유사도 value만 뽑아서 대입
+
+# 개인별로 계산된 예측 평점
+# ratings_pred_matrix
