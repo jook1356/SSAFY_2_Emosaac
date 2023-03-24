e642920ab983a2bd3eb553f94a053524dc94f245
kimjonghyeok <notify9637@naver.com>
2023-03-24 17:42:40 +0900

    [edit] interceptor 활용한 token 저장

--- client/src/components/test/Test.tsx
+++ client/src/components/test/Test.tsx
@@ -1,11 +1,18 @@
 import { useEffect, useState } from "react";
 import getGenreByTypeCode from "../../api/genre";
+import getMyInfo from "@/api/user/getMyInfo";
 export default function test() {
   //   const [webToonGenre, setWebToonGenre] = useState([]);
   useEffect(() => {
     const webtoon: number = 0;
     // const webnovel: number = 1;
     getGenreByTypeCode(webtoon).then((res) => {
+      const data = res;
+      console.log(data);
+    });
+  }, []);
+  useEffect(() => {
+    getMyInfo().then((res) => {
       const data = res;
       console.log(data);
     });
