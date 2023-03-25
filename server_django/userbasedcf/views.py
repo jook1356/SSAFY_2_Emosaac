d30d056170a01a81bd2f8e458019547393fbf585
gms9424 <gms9424@naver.com>
2023-03-26 04:00:42 +0900

    [Add] (대안) 나이,성별 기반 추천

--- server_django/userbasedcf/views.py
+++ server_django/userbasedcf/views.py
@@ -3,11 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render
 
-from userbasedcf import recommandWebtoon, recommandNovel
+from userbasedcf import recommandWebtoon, recommandNovel, recommandNovelByAgeAndGender, recommandWebtoonByAgeAndGender
 
 
 def index(request):
-    recommandWebtoon.execute_algorithm()
-    recommandNovel.execute_algorithm()
+    # recommandWebtoon.execute_algorithm()
+    # recommandNovel.execute_algorithm()
+    recommandNovelByAgeAndGender.execute_algorithm()
+    recommandWebtoonByAgeAndGender.execute_algorithm()
 
     return render(request, 'hi')
