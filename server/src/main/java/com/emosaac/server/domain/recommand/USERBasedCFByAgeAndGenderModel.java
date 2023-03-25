ec9989ef56984f9977609024f71008dd4348bea9
gms9424 <gms9424@naver.com>
2023-03-26 02:32:08 +0900

    [Add] 나이, 성별별 추천 모델 추가

--- server/src/main/java/com/emosaac/server/domain/recommand/USERBasedCFByAgeAndGenderModel.java
+++ server/src/main/java/com/emosaac/server/domain/recommand/USERBasedCFByAgeAndGenderModel.java
@@ -1,7 +1,6 @@
 package com.emosaac.server.domain.recommand;
 
 import com.emosaac.server.domain.BaseEntity;
-import com.emosaac.server.domain.book.Book;
 import com.emosaac.server.domain.user.User;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.Getter;
@@ -12,22 +11,21 @@ import javax.persistence.*;
 @Entity
 @Getter
 @Setter
-@Table(name="USER_BASED_CF_MODEL")
-public class USERBasedCFModel extends BaseEntity {
+@Table(name="USER_BASED_CF_BY_AGE_GENDER_MODEL")
+public class USERBasedCFByAgeAndGenderModel extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="ITEM_NO")
     private Integer id;
 
-    @JsonManagedReference
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name ="USER_NO")
-    private User user;
+    private Integer age;
+
+    private Integer gender;
 
     @JsonManagedReference
     private String bookNoList;
-
+    
     @Column(name="TYPE_CD")
     private Integer typeCode;
 }
