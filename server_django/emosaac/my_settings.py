49fb62b63b80f0242fdb919a3b69c308e56779d8
ai06010 <ai06010@naver.com>
2023-03-28 14:18:34 +0900

    [Fix] gitignore 수정

---
+++ server_django/emosaac/my_settings.py
@@ -0,0 +1,13 @@
+DATABASES = {
+  'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'emosaac',
+    'USER': 'emosaac',
+    'PASSWORD': 'nct127b1a4produce101',
+    'HOST': 'j8d203.p.ssafy.io',
+    'PORT': '3306',
+  }
+}
+
+
+SECRET_KEY = 'django-insecure-^*x*o224ey%-n8_twmkix#pqndbv5^3d2vhcjm^e3#^qhxv1!t'
