0f0820959ed85fe2b5930d632fbecd7186f61340
kimjonghyeok <notify9637@naver.com>
2023-04-03 11:24:17 +0900

    [edit] 유저수정 보완 완료, 프로필 이미지 수정됨, 중복확인 안 할 시 수정 불가
    Former-commit-id: ab74bd17eb3658ea3bc67791509d2a9b4e963126

--- client/src/pages/mypage/index.tsx
+++ client/src/pages/mypage/index.tsx
@@ -1,6 +1,7 @@
 import MyPage from "@/components/mypage/MyPage";
 
 export default function mypage({ myInfo }: any) {
+  // console.log(myInfo);
   return (
     <>
       <MyPage myinfo={myInfo} />
