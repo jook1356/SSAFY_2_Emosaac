75bea002267f81a2ed08cdec45dfedc0787256b8
kimjonghyeok <notify9637@naver.com>
2023-04-04 17:05:47 +0900

    [add] 읽음 리스트 추가중
    Former-commit-id: 4ec6952d26d29dc17e0e6e1333ad33d78434179e

--- client/src/components/mypage/AlreadyReadList.tsx
+++ client/src/components/mypage/AlreadyReadList.tsx
@@ -6,57 +6,58 @@ import { useState } from "react";
 import { useIsResponsive } from "@/components/Responsive/useIsResponsive";
 import { useRouter } from "next/router";
 import ScrollableCarousel from "../UI/HorizontalScroll/backup/ScrollableCarousel";
-type BookMarkProps = {
+import getAlreadyRead from "@/api/user/getAlreadyRead";
+type AlreadyProps = {
   typeCode: number;
 };
-type returnGetBookMarkProps = {
+type returnGetAlreadyProps = {
   bookId: number;
   thumbnail: string;
   modifiedDate: string;
   prevTime: string;
 };
-const BookMark = ({ typeCode }: BookMarkProps) => {
-  const getBookMarkAPI = ({
+const AlreadyReadList = ({ typeCode }: AlreadyProps) => {
+  const getAlreadyListkAPI = ({
     bookList,
     size,
   }: {
-    bookList: returnGetBookMarkProps[];
+    bookList: returnGetAlreadyProps[];
     size: number;
   }) => {
     const prevId = bookList.length ? bookList[bookList.length - 1].bookId : 0;
     const prevTime = bookList.length
       ? bookList[bookList.length - 1].prevTime
       : "";
-    return getBookMark(prevId, prevTime, size, typeCode);
+    return getAlreadyRead(prevId, prevTime, size, typeCode);
   };
   const router = useRouter();
   const [isDeskTop, isTablet, isMobile] = useIsResponsive();
   const [prevId, setPrevId] = useState<number>(0);
   const [prevTime, setPrevTime] = useState<string>("0");
   const [size, setSize] = useState<number>(7);
-  const [bookmarks, setBookmarks] = useState<returnGetBookMarkProps[]>([]);
+  const [alreadyLists, setAlreadyLists] = useState<returnGetAlreadyProps[]>([]);
   useEffect(() => {
     getBookMark(prevId, prevTime, size, typeCode).then((res) => {
       const data = res;
       if (data !== null) {
         // console.log(data);
-        setBookmarks(data.content);
+        setAlreadyLists(data.content);
       }
     });
   }, [typeCode]);
   return (
     <section css={bookmarkwrapCSS}>
       <h3>북마크 목록</h3>
-      {bookmarks && bookmarks.length > 0 ? (
+      {alreadyLists && alreadyLists.length > 0 ? (
         <div>
           <ScrollableCarousel
-            API={getBookMarkAPI}
+            API={getAlreadyListkAPI}
             identifier={"북마크리스트"}
           />
         </div>
       ) : (
         <div css={nobookmarkCSS}>
-          북마크 한 {typeCode === 0 ? "웹툰" : "웹소설"}이 없어요
+          읽은 {typeCode === 0 ? "웹툰" : "웹소설"}이 없어요
         </div>
       )}
     </section>
@@ -65,7 +66,7 @@ const BookMark = ({ typeCode }: BookMarkProps) => {
 const bookmarkwrapCSS = css`
   display: flex;
   flex-direction: column;
-  padding-left: 30px;
+  padding-left: 20px;
   margin-right: 50px;
   width: 100%;
   & > h3 {
@@ -110,4 +111,4 @@ const nobookmarkCSS = css`
   justify-content: center;
   align-items: center;
 `;
-export default BookMark;
+export default AlreadyReadList;
