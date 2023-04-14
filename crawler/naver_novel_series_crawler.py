399a439c94badf8a885fef75d4bb805e552b73a2
gms9424 <gms9424@naver.com>
2023-04-14 10:09:45 +0900

    [Feat] 네이버 크롤링 코드 추가

---
+++ crawler/naver_novel_series_crawler.py
@@ -0,0 +1,172 @@
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from time import sleep
+import pandas as pd
+import random
+
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
+#크롬 드라이버 자동 업데이트
+from webdriver_manager.chrome import ChromeDriverManager
+
+#브라우저 꺼짐 방지
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+
+#불필요한 에러 메시지 없애기
+chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
+
+
+URL = 'https://series.naver.com/novel/categoryProductList.series?categoryTypeCode=genre&'
+html = requests.get(URL).text # html 문서 전체를 긁어서 출력해줌, .text는 태그 제외하고 text만 출력되게 함
+soup = BeautifulSoup(html, 'html.parser')
+
+id_list = [] ; title_list = [] ; author_list = [] ; day_list = [] ; genre_list = [] ; 
+story_list = [] ; platform_list = []; img_list=[]; amount_list=[]; score_list=[]; isDoing_list=[]; start_date_list=[]
+tag_list = []; viewer_list = []; grade_list = []; num = 0; href_list=[]
+
+genreCode = ['201', '207', '202', '208', '206', '203', '205']
+service = Service(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(r'C:\ssafy\특화프로젝트\crawling\chromedriver', options = chrome_options)
+
+p = 0;
+
+for i in range(len(genreCode)):
+# for i in range(1):
+
+    sleep(0.5) # 크롤링 중간 중간 텀을 주어 과부하 생기지 않도록
+
+    for p in range(1, 31):
+        # p = p+1
+        # try:
+
+            if p==29 and i==5:
+                    break
+            
+            driver.get(URL+f'genreCode={genreCode[i]}'+'&page='+str(p)) 
+
+            
+            for j in range(1, 26):
+
+                if p==28 and i==5 and j==14:
+                    break
+                page = driver.find_element(By.XPATH,f'//*[@id="content"]/div/ul/li[{j}]')
+
+
+                sleep(0.5)
+
+                href = page.find_element(By.TAG_NAME,'a').get_attribute('href')
+                driver.get(href)
+
+                html = driver.page_source
+                soup = BeautifulSoup(html, 'html.parser') # 이동한 페이지 주소 읽고 파싱
+
+                try:
+                    title = driver.find_element(By.XPATH,'//*[@id="content"]/div[1]/h2').text.replace("독점","네이버 독점")
+                    if '단행본' in title:
+                          driver.back()
+                          continue
+
+                except:
+                    driver.back()
+                    continue
+
+                   
+                try:
+                    driver.find_element(By.XPATH,'//*[@id="content"]/div[2]/div[1]/span/a').click()
+                except:
+                    story = driver.find_element(By.XPATH,'//*[@id="content"]/div[2]/div').text.replace("접기", "")
+
+                
+                try:
+                    story = driver.find_element(By.XPATH, '//*[@id="content"]/div[2]/div[2]').text.replace("접기", "")
+                except:
+                    story = driver.find_element(By.XPATH,'//*[@id="content"]/div[2]/div').text.replace("접기", "")
+
+                story_list.append(story)
+                print(story)
+
+                           
+                print(title)
+                title_list.append(title)
+        
+                try:
+                    img = driver.find_element(By.XPATH,'//*[@id="container"]/div[1]/a/img').get_attribute('src').replace('?type=m260','')
+                except:
+                    img = driver.find_element(By.XPATH,'//*[@id="container"]/div[1]/span/img').get_attribute('src').replace('?type=m260','')
+                        
+                print(img)
+                img_list.append(img)
+               
+                
+                amount = driver.find_element(By.XPATH,'//*[@id="content"]/h5/strong').text
+                print("amount "+ amount)
+                amount_list.append(amount);
+
+                    
+                id_list.append(num) ; num += 1  # id 리스트에 추가
+                print(num)
+
+                platform_list.append('네이버') # 플랫폼 리스트에 추가
+
+
+                author = driver.find_element(By.XPATH,'//*[@id="content"]/ul[1]/li/ul/li[3]/a').text
+                print(author)
+                author_list.append(author)
+
+                score = driver.find_element(By.XPATH,'//*[@id="content"]/div[1]/div[1]/em').text
+                print(score)
+                score_list.append(score)
+
+                isDoing = driver.find_element(By.XPATH,'//*[@id="content"]/ul[1]/li/ul/li[1]/span').text
+                print(isDoing)
+                isDoing_list.append(isDoing)
+
+                href_list.append(href)
+
+                    
+                genre = driver.find_element(By.XPATH,'//*[@id="content"]/ul[1]/li/ul/li[2]/span/a').text
+                print(genre)
+                genre_list.append(genre)
+
+                tag_list.append(genre)  
+
+                viewer_list.append(random.randrange(3, 8))
+
+                sleep(0.5)             
+
+                try:
+                    start_date = driver.find_element(By.XPATH, '//*[@id="volumeList"]/tr[1]/td[1]/div/em').text.replace("(","").replace(")","")
+                except:
+                    start_date = driver.find_element(By.XPATH, '//*[@id="volumeList"]/tr[1]/td[1]/div/a/em').text.replace("(","").replace(")","")
+                print(start_date[:-1])
+                start_date_list.append(start_date[:-1])
+                driver.back() # 뒤로 가기
+        # except Exception as e:
+        #         driver.back() # 뒤로 가기
+
+        #     p = 0
+        #     break
+
+cols = []
+total_data = pd.DataFrame(columns = cols)
+total_data['id'] = id_list
+total_data['title'] = title_list
+total_data['img'] = img_list
+total_data['author'] = author_list
+total_data['day'] = isDoing_list
+total_data['genre'] = genre_list
+total_data['story'] = story_list
+total_data['tag'] = tag_list
+total_data['viewer'] = viewer_list
+total_data['grade'] = score_list
+total_data['regist'] = start_date_list
+total_data['series'] = amount_list
+total_data['href'] = href_list
+total_data['platform'] = platform_list
+total_data.to_excel(r'C:\ssafy\특화프로젝트\crawling\naver\네이버웹소설_href추가.xlsx',  sheet_name = '네이버 웹소설', encoding = 'utf-8-sig')
+
---
+++ crawler/naver_weebtoon_cralwer.py
@@ -0,0 +1,244 @@
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from time import sleep
+import pandas as pd
+from selenium.webdriver.common.keys import Keys
+import pyautogui
+import pyperclip
+import time
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
+# 크롬 드라이버 자동 업데이트
+from webdriver_manager.chrome import ChromeDriverManager
+
+# 브라우저 꺼짐 방지
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+
+# 불필요한 에러 메시지 없애기
+chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
+
+URL = 'https://comic.naver.com/webtoon'
+html = requests.get(URL).text  # html 문서 전체를 긁어서 출력해줌, .text는 태그 제외하고 text만 출력되게 함
+soup = BeautifulSoup(html, 'html.parser')
+
+week = ['tab=mon', 'tab=tue', 'tab=tue', 'tab=wed', 'tab=fri', 'tab=sat', 'tab=sun', 'tab=finish']
+ko_week = ['월', '화', '수', '목', '금', '토', '일', '완결']
+
+id_list = [];
+title_list = [];
+author_list = [];
+day_list = [];
+genre_list = [];
+story_list = [];
+platform_list = [];
+img_list = [];
+tag_list = [];
+grade_list = [];
+viewer_list = [];
+series_list = [];
+regist_list = [];
+href_list1 = []
+num = 1
+
+driver = webdriver.Chrome(r'C:\ssafy\특화프로젝트\crawling\chromedriver', options=chrome_options)
+
+
+def naver_login():
+    login = 'https://nid.naver.com/nidlogin.login?url=https%3A%2F%2Fcomic.naver.com%2Fwebtoon%3Ftab%3Dmon'
+    driver.implicitly_wait(2)
+    driver.maximize_window()
+    driver.get(login)
+
+    driver.find_element_by_xpath('//*[@id="id"]').click()
+    pyperclip.copy('{아이디}')
+    pyautogui.hotkey('ctrl', 'v')
+    sleep(5)
+
+    driver.find_element_by_xpath('//*[@id="pw"]').click()
+    pyperclip.copy('{비밀번호}')
+    pyautogui.hotkey('ctrl', 'v')
+    sleep(5)
+
+    # 로그인 버튼
+    login_send = driver.find_element_by_xpath('//*[@id="log.login"]')
+    login_send.click()
+
+
+driver.get(URL)
+driver.implicitly_wait(3)  # 로딩이 끝날 때까지 10초까지는 기다려줌
+naver_login()  # 로그인
+
+for i in range(7, 8):
+    sleep(0.5)
+    uri = URL + "?" + week[i]
+
+    driver.get(uri)  # 요일별로 링크 가져옴
+
+
+    sleep(0.5)
+
+    # 스크롤 전 높이
+    before_h = driver.execute_script("return window.scrollY")
+
+    # 무한 스크롤
+    while True:
+
+        sleep(3)
+
+        # 맨 아래로 스크롤을 내린다
+        driver.find_element_by_css_selector("body").send_keys(Keys.END)
+
+        # 스크롤 사이 페이지 로딩 시간
+        sleep(3)
+
+        # 스크롤 후 높이
+        after_h = driver.execute_script("return window.scrollY")
+
+        if after_h == before_h:
+            break
+
+        sleep(3)
+
+        before_h = after_h
+
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')  # 이동한 페이지 주소 읽고 파싱
+
+    sleep(1)
+
+    href_list = driver.find_elements_by_xpath('//*[@id="content"]/div[1]/ul/li/a')
+    grades = driver.find_elements_by_xpath('//*[@id="content"]/div[1]/ul/li/div/div/span')
+    authors_list = soup.find_all('a', {'class': 'ContentAuthor__author--CTAAP'})
+
+    page_list = []
+    for page in href_list:
+        page_list.append(page.get_attribute('href'))
+
+    scores = []
+    for grade in grades:
+        scores.append(grade.text.replace("별점", "").replace("\n", ""))
+
+    authors = []
+    for a in authors_list:
+        authors.append(a.text)
+        print(a.text)
+
+    idx = 0
+
+    print(len(page_list))
+
+    for href in page_list:
+
+        sleep(0.5)
+
+        href_list1.append(href)
+        print(href)
+
+        sleep(0.5)
+
+        driver.get(href + "&page=1&sort=ASC")  # 디테일 이동
+
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'html.parser')  # 이동한 페이지 주소 읽고 파싱
+
+        sleep(2)
+
+        day = ko_week[i]
+
+        try:
+            title = driver.find_element_by_xpath('//*[@id="content"]/div[1]/div/h2').text.replace('휴재', '')
+            print(title)
+            if (title in title_list):  # 요일 두 개 이상이면 요일만 추가함
+                day_list[title_list.index(title)] += ' ' + day
+                driver.back()
+                continue
+        except:
+            driver.back()
+
+        title_list.append(title)
+
+        grade_list.append(scores[idx])
+
+        author_list.append(authors[idx])
+        print(authors[idx])
+
+        id_list.append(num);
+        print(f"num {num}")
+        num += 1  # id 리스트에 추가
+
+        sleep(0.2)
+
+        first = driver.find_element_by_xpath('//*[@id="content"]/div[3]/ul/li[1]/a/div[2]/div/span[2]').text
+        print(first)
+        regist_list.append("20" + first)
+
+        sleep(2)
+
+        ## 이미지
+        try:
+            thumb = driver.find_element_by_xpath('//*[@id="content"]/div[1]/button/div/img').get_attribute('src')
+        except:
+            try:
+                thumb = driver.find_element_by_xpath('//*[@id="content"]/div[1]/button/div/img').get_attribute('src')
+            except:
+                thumb = driver.find_element_by_xpath('//*[@id="content"]/div[1]/button/div/img').get_attribute('src')
+
+        print(thumb)
+        img_list.append(thumb)
+
+        ## 요일
+        day_list.append(day)
+
+        ## 줄거리
+        story = driver.find_element_by_xpath('//*[@id="content"]/div[1]/div/div[2]/p').text
+        #         print(story)
+        story_list.append(story)
+
+        ## 장르 & 태그
+        tags = driver.find_elements_by_xpath('//*[@id="content"]/div[1]/div/div[2]/div/div/a')
+        temp = ""
+        genre = tags[0].text.replace("#", "")
+        genre_list.append(genre)
+
+        for tag in tags:
+            temp += tag.text.replace("#", "") + " "
+        tag_list.append(temp)
+
+        viewer = driver.find_element_by_xpath('//*[@id="content"]/div[2]/div/button[1]/span[2]').text
+        viewer_list.append(viewer)
+
+        try:
+            series = driver.find_element_by_xpath('//*[@id="content"]/div[3]/div[1]/div[1]').text
+        except:
+            series = driver.find_element_by_xpath('//*[@id="content"]/div[3]/div[2]/div[1]').text
+        series = series.replace("총 ", "")
+        series = series.replace("화", "")
+        series_list.append(series)
+
+        platform_list.append("네이버")
+
+        driver.back()
+
+        idx += 1
+
+total_data = pd.DataFrame()
+total_data['id'] = id_list
+total_data['title'] = title_list
+total_data['img'] = img_list  ## 썸네일
+total_data['author'] = author_list
+total_data['day'] = day_list
+total_data['genre'] = genre_list
+total_data['story'] = story_list
+total_data['tag'] = tag_list
+total_data['viewer'] = viewer_list
+total_data['grade'] = grade_list
+total_data['regist'] = regist_list
+total_data['series'] = series_list
+total_data['href'] = href_list1
+total_data['platform'] = platform_list
+total_data.to_excel(r'C:\ssafy\특화프로젝트\crawling\naver\네이버웹툰_완결_1760_new.xlsx', sheet_name='네이버 웹툰', encoding='utf-8-sig')
